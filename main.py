e21f4122ee89dd42a22ce3e002df128615a8bfef
Bruno <susurrus@tuta.com>
2025-01-07 01:57:46 +0100

    Finished player movement

--- main.py
+++ main.py
@@ -11,19 +11,30 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            
-        player.update(dt)
+        
+        for object in updatable:
+            object.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
