e63af844eda68162f9725731cfbab8f5bd3f7448
Bruno <susurrus@tuta.com>
2025-01-07 02:25:32 +0100

    Started work on asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
 
 
 def main():
@@ -14,8 +15,10 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
